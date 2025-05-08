f31901313a478d96f91c05aa9a7d2128de6c723c
Mohsin Khan <209888552+BayHatter@users.noreply.github.com>
2025-05-08 20:15:50 +0500

    Update and rename eth.js to wan.js

--- pages/wan.js
+++ pages/wan.js
@@ -1,15 +1,15 @@
 import dynamic from 'next/dynamic';
 import WalletProvider from '../assets/WalletProvider';
 import TopMenu from '../assets/TopMenu';
-import Ethereum from '../assets/Ethereum';
+import Wanchain from '../assets/Wanchain';
 import Sidebar from '../assets/Sidebar';
 import Head from 'next/head';
 
-const EthereumWithNoSSR = dynamic(() => import('../assets/Ethereum'), {
+const WanchainWithNoSSR = dynamic(() => import('../assets/Wanchain'), {
   ssr: false,
 });
 
-export default function Eth() {
+export default function Wan() {
   return (
     <div className="min-h-screen bg-white-100">
       <WalletProvider>
@@ -18,17 +18,17 @@ export default function Eth() {
             <TopMenu account={account} connectWallet={connectWallet} disconnectWallet={disconnectWallet} />
             
             <Head>
-              <title>Ethereum Token Creation Tool</title>
-              <meta name="description" content="Create and launch your own meme token on Ethereum" />
-              <meta property="og:title" content="Launch Your Meme Coin on Ethereum" />
-              <meta property="og:description" content="Build and deploy a meme coin instantly on Ethereum. Fast, secure, and 100% code-free." />
+              <title>Wanchain Token Creation Tool</title>
+              <meta name="description" content="Create and launch your own meme token on Wanchain" />
+              <meta property="og:title" content="Launch Your Meme Coin on Wanchain" />
+              <meta property="og:description" content="Build and deploy a meme coin instantly on Wanchain. Fast, secure, and 100% code-free." />
               <meta property="og:type" content="website" />
-              <meta property="og:url" content="/eth" />
+              <meta property="og:url" content="/wan" />
             </Head>
 
             <div className="container mx-auto p-4 max-w-7xl flex flex-col md:flex-row gap-4">
               <div className="flex-1">
-                <EthereumWithNoSSR provider={provider} account={account} connectWallet={connectWallet} />
+                <WanchainWithNoSSR provider={provider} account={account} connectWallet={connectWallet} />
               </div>
               <div className="md:w-[300px] flex-shrink-0">
                 <Sidebar />
